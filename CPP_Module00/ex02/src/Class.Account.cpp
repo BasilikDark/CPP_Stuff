1c5e073d741de208f0c717620e1207b5ed35636f
Raphael Rupp <rrupp@c4r7p1.42vienna.com>
2023-06-03 12:07:51 +0200

    I think I was able to recreate the Class.Account.cpp as required and compared the output with the sourcefile! :)

--- CPP_Module00/ex02/src/Class.Account.cpp
+++ CPP_Module00/ex02/src/Class.Account.cpp
@@ -6,14 +6,14 @@
 /*   By: rrupp <rrupp@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 15:32:28 by rrupp             #+#    #+#             */
-/*   Updated: 2023/06/02 15:37:10 by rrupp            ###   ########.fr       */
+/*   Updated: 2023/06/03 12:02:57 by rrupp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
 #include <ctime>
-# include <iomanip>
+#include <iomanip>
 
 Account::Account(int initial_deposit)
 {
@@ -55,7 +55,7 @@ void	Account::displayAccountsInfos(void)
 {
 	_displayTimestamp();
 	std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount()
-	<< ";deposits:" << _totalNbDeposits << ";withdrawls:" << _totalNbWithdrawals << std::endl;
+	<< ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 void	Account::makeDeposit(int deposit)
@@ -66,7 +66,7 @@ void	Account::makeDeposit(int deposit)
 	this->_amount += deposit;
 	_totalAmount += deposit;
 	_totalNbDeposits++;
-	std::cout << ";amount:" << this->_amount << "nb_deposits;" << this->_nbDeposits << std::endl;
+	std::cout << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits << std::endl;
 }
 
 bool	Account::makeWithdrawal(int withdrawl)
@@ -74,7 +74,7 @@ bool	Account::makeWithdrawal(int withdrawl)
 	if (this->_amount - withdrawl < 0)
 	{
 		_displayTimestamp();
-		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawl:refused" << std::endl;
+		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:refused" << std::endl;
 		return (false);
 	}
 	else
@@ -85,7 +85,7 @@ bool	Account::makeWithdrawal(int withdrawl)
 		this->_amount -= withdrawl;
 		_totalAmount -= withdrawl;
 		_totalNbWithdrawals++;
-		std::cout << ";amount:" << this->_amount << ";nb_withdrawls:" << this->_nbWithdrawals <<std::endl;
+		std::cout << ";amount:" << this->_amount << ";nb_withdrawals:" << this->_nbWithdrawals <<std::endl;
 		return (true);
 	}
 }
@@ -97,12 +97,20 @@ int	Account::checkAmount(void) const
 
 void	Account::displayStatus(void) const
 {
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount
+	<< ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
 	return ;
 }
 
 void	Account::_displayTimestamp(void)
 {
-	std::cout << "[" << (time(NULL) / 60) << "_" << std::cout.fill('0') << std::setw(5) << (time(NULL) % 10000) <<  "] ";
+	char	buffer[16];
+	std::time_t currentTime = std::time(NULL);
+	std::tm* timeInfo = std::localtime(&currentTime);
+	std::strftime(buffer, sizeof(buffer), "%Y%m%d_%H%M%S", timeInfo);
+	std::string	formattedTime(buffer);
+	std::cout <<  "[" << formattedTime << "] ";
 }
 
 int Account::_nbAccounts = 0;
