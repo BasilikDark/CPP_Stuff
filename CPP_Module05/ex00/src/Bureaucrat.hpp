a78b3db961a109d86af5d36af0d5d038b581e9c4
Raphael Rupp <rrupp@c2r7p6.42vienna.com>
2023-08-08 09:17:58 +0200

    added the start of CPP_Module05 :)

---
+++ CPP_Module05/ex00/src/Bureaucrat.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rrupp <rrupp@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/05 15:13:48 by rrupp             #+#    #+#             */
+/*   Updated: 2023/08/05 15:15:06 by rrupp            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREUCRAT_HPP
+# define BUREUCRAT_HPP
+
+class Bureaucrat
+{
+	public:
+	 Bureaucrat(/* args */);
+	~Bureaucrat();
+private:
+};
+
+Bureaucrat::Bureaucrat(/* args */)
+{
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
+
+#endif
